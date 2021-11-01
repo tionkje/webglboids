48bcf158ecb1adf25377dc263dd2ec2a97f13195
Bastiaan Tiobook <deknudtb@gmail.com>
2021-11-01 10:12:34 +0100

    moved function to geoutil

--- src/shapes.js
+++ src/shapes.js
@@ -1,19 +1,7 @@
 import undent from './undent.js';
 import { createProgram, getProgramSetters } from "./glUtil.js"
+import { lineSegmentToQuad } from './geoUtil.js';
 
-import "../node_modules/gl-matrix/gl-matrix.js";
-const { vec3 } = glMatrix;
-
-function lineSegmentToQuad(start,end,width){
-  var dir = vec3.sub(vec3.create(),end,start);
-  var up = vec3.fromValues(dir[1],-dir[0],dir[2]);
-  vec3.normalize(up,up);
-  var up = vec3.scale(up, up, width);
-  var down = vec3.scale(vec3.create(), up, -1);
-  var up2 = vec3.add(vec3.create(), up, dir);
-  var down2 = vec3.add(vec3.create(), down, dir);
-  return [up, up2, down, down2].map(x=>vec3.add(x,x,start));
-}
 
 export const S_CIRCLE=0;
 export const S_RECT=1;
