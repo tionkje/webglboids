8c37defbdb32a87d89e54ffa876f208958e191f1
Bastiaan Tiobook <deknudtb@gmail.com>
2021-11-04 21:57:39 +0100

    remove comment

--- src/geoUtil.js
+++ src/geoUtil.js
@@ -6,14 +6,8 @@ const { max, min } = Math;
 export function lineSegmentToQuad(start,end,width){
   var dir = vec3.sub(vec3.create(),end,start);
   return vectorToQuad(start, dir, width);
-  // var up = vec3.fromValues(dir[1],-dir[0],dir[2]);
-  // vec3.normalize(up,up);
-  // var up = vec3.scale(up, up, width);
-  // var down = vec3.scale(vec3.create(), up, -1);
-  // var up2 = vec3.add(vec3.create(), up, dir);
-  // var down2 = vec3.add(vec3.create(), down, dir);
-  // return [up, up2, down, down2].map(x=>vec3.add(x,x,start));
 }
+
 export function vectorToQuad(start,dir,width){
   var up = vec3.fromValues(dir[1],-dir[0],dir[2]);
   vec3.normalize(up,up);
